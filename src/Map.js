369743b2dc9d7f49e25e9627fad9c05f67c8b868
alfonsotech <alfonsotech@nym.hush.com>
2018-05-18 11:48:45 -0400

    Adding markers to map from stores list

--- src/Map.js
+++ src/Map.js
@@ -19,54 +19,58 @@ const divStyle = {
 
 const InitMap = withGoogleMap(props =>
   <GoogleMap
+    ref={props.onMapLoad}
     defaultZoom={11}
     defaultCenter={{ lat: 19.432608, lng: -99.133209 }}
   >
-    {/* {this.state.stores.map( (store, i) => {
-      return <Marker lat={store.results[0].geometry.location.lat} lng={store.results[0].geometry.location.lng} />
+    {/* {props.markers.map( (marker, i) => {
+      <Marker
+        key={i}
+        position={ {lat: 19.405274529, lng: -99.181835108}}
+     />
     })} */}
-    <Marker
-      position={{ lat: 19.28786, lng: -99.65324 }}
-    />
+    {/* <Marker
+      lat={19.405274529} lng={-99.181835108}
+    /> */}
+    <Marker lat={19.28786} lng={-99.65324}/>
   </GoogleMap>
 );
 
 export default class Map extends Component {
 
-  constructor() {
-    super()
-    this.state = {
-      stores: [],
-      markers: [
-        {position: { lat: 19.405274529, lng: -99.181835108 }},
-        {position: { lat: 19.403979246, lng: -99.122440271 }}
-      ]
-    }
-  }
-
-  componentWillMount = () => {
-    axios.get('../store_directory.json')
-    .then( response => {
-
-      response.data.map( (store, i) => {
-
-        let url = 'https://maps.googleapis.com/maps/api/geocode/json?key=' + keys.googleMapsKey.apiKey + '&address="'+ store.Address + '"'
-
-        axios.get(url)
-        .then( response => {
-          this.setState({
-            stores: this.state.stores.concat(response.data)
-          })
-
-        })
-        .catch(err => console.log(err))
-      })
-    })
-    .catch(err => console.log(err))
-  }
+  // constructor(props) {
+  //   super(props)
+  //   this.state = {
+  //     stores:[],
+  //     markers: [
+  //       {position: { lat: 19.405274529, lng: -99.181835108 }}
+  //     ]
+  //   }
+  // }
+  //
+  // componentWillMount = () => {
+  //   axios.get('../store_directory.json')
+  //   .then( response => {
+  //
+  //     response.data.map( (store, i) => {
+  //
+  //       let url = 'https://maps.googleapis.com/maps/api/geocode/json?key=' + keys.googleMapsKey.apiKey + '&address="'+ store.Address + '"'
+  //
+  //       axios.get(url)
+  //       .then( response => {
+  //         this.setState({
+  //           stores: this.state.stores.concat(response.data)
+  //         })
+  //
+  //       })
+  //       .catch(err => console.log(err))
+  //     })
+  //   })
+  //   .catch(err => console.log(err))
+  // }
 
   render() {
-    console.log(this.state.stores);
+    // console.log(this.state.stores);
     const { markers, center, zoom } = this.props
     const url = 'https://maps.googleapis.com/maps/api/js?key='+keys.googleMapsKey.apiKey+'&v=3.exp&libraries=geometry,drawing,places'
     return (
